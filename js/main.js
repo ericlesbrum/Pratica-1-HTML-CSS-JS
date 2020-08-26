ef662ffdf202b9fa633e6432d7512726b0571106
Ericles Brum <35080587+Frakren@users.noreply.github.com>
2020-08-26 20:27:00 -0300

    Refatorar

--- js/main.js
+++ js/main.js
@@ -19,12 +19,3 @@ function checkCredential(){
         return false;
     }
 }
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
