61286f571c285ccae873432933720ec903d4442c
Ericles Brum <35080587+Frakren@users.noreply.github.com>
2020-08-27 13:03:08 -0300

    Redirecionar paginas
    Abrir uma pagina dentro de uma div

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-document.querySelector("#alert").style.display="none";
+setAlertNone();
+setInterval(setLocalTime);
 $(function(){
     $("#checkIDPass").click(function(){
         if(checkCredential()){
@@ -7,8 +8,12 @@ $(function(){
         else{
             $("#alert").show();
         }
-    })
+    });
 });
+function setAlertNone(){
+    if(document.querySelector("#alert")!=null)
+        document.querySelector("#alert").style.display="none";
+}
 function checkCredential(){
     let id=document.querySelector("#idUser").value;
     let psword=document.querySelector("#inputPassword").value;
@@ -17,5 +22,28 @@ function checkCredential(){
     }
     else{
         return false;
+    }
+}
+function redirect(pos) {
+    switch(pos){
+        case 0:
+            document.querySelector(".main").innerHTML='<object type="text/html" data="storage.html" ></object>';
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
+}
+function setLocalTime() {
+    if(document.getElementById("timer")!=null){
+        var d = new Date();
+        document.getElementById("timer").innerHTML = d.toLocaleTimeString();
     }
 }
